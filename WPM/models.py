561d4363d9f71804f85510f009e7f1a7c6db6be9
Ryszard <Yellow-Magic-Duck-of-War@noreply.github.com>
2021-01-14 14:42:34 +0100

    Zmiany:
    'models.py':
    * dodałem zadanie 8.

--- WPM/models.py
+++ WPM/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
+# ĆWICZENIE 8
+"""
+    1. Do wybranych dwóch modeli dodaj właściciela, jako użytkownika wbudowanego w Django
+    2. Ustaw różne opcje praw dostępu dla poszczególnych modeli -
+       tak, aby były w miarę sensowne
+    3. Przy dodawaniu rekordów do modeli z właścicielem -
+       ustaw automatycznie właściciela jako użytkownika, który dany rekord stworzył
+    4. Dodaj widoki dla użytkowników bez możliwości modyfikacji
+    5. Napisz odpowiednie testy przynajmniej dla dwóch modeli
+"""
+
 """
 Może się przydać do uprawmnieniń użytkownika.
 
